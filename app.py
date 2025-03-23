4fc1ed24fa000e08d1ccb086d9c089da472e519e
Todd Sproull <todd@wustl.edu>
2025-03-23 08:10:03 -0500

    new message

--- app.py
+++ app.py
@@ -6,11 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     return jsonify({
-<<<<<<< HEAD
         "message": "Hello Cloud Computing",
-=======
-        "message": "Hello Cloud Different Message!",
->>>>>>> 1ca0a1a2edb7dabb581f299e8d9b198d08af50c6
         "status": "success"
     })
 
